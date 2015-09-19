e85150d1322308f964d775fcf7350f394eb14131
MahmoudBadr <mahmoudbadr007@gmail.com>
2015-09-19 02:13:08 +0200

    19092015

--- app/src/main/java/app/Nursery/AddNewStudent.java
+++ app/src/main/java/app/Nursery/AddNewStudent.java
@@ -1,10 +1,9 @@
-package app.mysqlapp;
+package app.Nursery;
 /***********
 ParallelCodes.com
 *************/
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.ActionBarActivity;
@@ -17,15 +16,14 @@ import android.widget.TextView;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class Login extends ActionBarActivity {
+public class AddNewStudent extends ActionBarActivity {
 
-    Button loginbtn;
+    Button add;
     TextView errorlbl;
-    EditText edname, edpassword;
+    EditText name, address, pincode;
     java.sql.Connection connect;
     PreparedStatement preparedStatement;
     Statement st;
@@ -33,7 +31,7 @@ public class Login extends ActionBarActivity {
 
     @SuppressLint("NewApi")
     private Connection ConnectionHelper(String user, String password,
-                                        String database, String server ) {
+                                        String database, String server) {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
                 .permitAll().build();
         StrictMode.setThreadPolicy(policy);
@@ -58,36 +56,37 @@ public class Login extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.new_student);
 
-        loginbtn = (Button) findViewById(R.id.btnlogin);
+        add = (Button) findViewById(R.id.btnadd);
 
         errorlbl = (TextView) findViewById(R.id.lblerror);
 
-        edname = (EditText) findViewById(R.id.txtname);
-        edpassword = (EditText) findViewById(R.id.txtpassword);
+        name = (EditText) findViewById(R.id.txtname);
+        address = (EditText) findViewById(R.id.txtaddress);
+        pincode = (EditText) findViewById(R.id.txtpincode);
 
         ipaddress = "192.168.1.7:1433";
         db = "Nursery";
         username = "sa";
         password = "P@ssw0rd";
         connect = ConnectionHelper(username, password, db, ipaddress);
-        loginbtn.setOnClickListener(new View.OnClickListener() {
+        add.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 try {
-
                     st = connect.createStatement();
-                    ResultSet rs = st.executeQuery("select * from login where userid='" + edname.getText().toString() + "' and password='" + edpassword.getText().toString() + "'");
-                    if (rs != null && rs.next()) {
-                        Intent i = new Intent(Login.this, MainActivity.class);
-                        startActivity(i);
-                    } else {
-                        errorlbl.setText("Sorry, wrong credentials!!!");
-                    }
 
-
+                    preparedStatement = connect
+                            .prepareStatement("insert into studentRecord(Name,Address,Pincode) values ('"
+                                    + name.getText().toString()
+                                    + "','"
+                                    + address.getText().toString()
+                                    + "','"
+                                    + pincode.getText().toString() + "')");
+                    preparedStatement.executeUpdate();
+                    errorlbl.setText("Data Added successfully");
                 } catch (SQLException e) {
                     errorlbl.setText(e.getMessage().toString());
                 }
--- app/src/main/java/app/Nursery/Login.java
+++ app/src/main/java/app/Nursery/Login.java
@@ -1,9 +1,10 @@
-package app.mysqlapp;
+package app.Nursery;
 /***********
 ParallelCodes.com
 *************/
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.ActionBarActivity;
@@ -16,14 +17,15 @@ import android.widget.TextView;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class MainActivity extends ActionBarActivity {
+public class Login extends ActionBarActivity {
 
-    Button add;
+    Button loginbtn;
     TextView errorlbl;
-    EditText name, address, pincode;
+    EditText edname, edpassword;
     java.sql.Connection connect;
     PreparedStatement preparedStatement;
     Statement st;
@@ -31,7 +33,7 @@ public class MainActivity extends ActionBarActivity {
 
     @SuppressLint("NewApi")
     private Connection ConnectionHelper(String user, String password,
-                                        String database, String server) {
+                                        String database, String server ) {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
                 .permitAll().build();
         StrictMode.setThreadPolicy(policy);
@@ -56,37 +58,36 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
 
-        add = (Button) findViewById(R.id.btnadd);
+        loginbtn = (Button) findViewById(R.id.btnlogin);
 
         errorlbl = (TextView) findViewById(R.id.lblerror);
 
-        name = (EditText) findViewById(R.id.txtname);
-        address = (EditText) findViewById(R.id.txtaddress);
-        pincode = (EditText) findViewById(R.id.txtpincode);
+        edname = (EditText) findViewById(R.id.txtname);
+        edpassword = (EditText) findViewById(R.id.txtpassword);
 
         ipaddress = "192.168.1.7:1433";
         db = "Nursery";
         username = "sa";
         password = "P@ssw0rd";
         connect = ConnectionHelper(username, password, db, ipaddress);
-        add.setOnClickListener(new View.OnClickListener() {
+        loginbtn.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 try {
-                    st = connect.createStatement();
 
-                    preparedStatement = connect
-                            .prepareStatement("insert into studentRecord(Name,Address,Pincode) values ('"
-                                    + name.getText().toString()
-                                    + "','"
-                                    + address.getText().toString()
-                                    + "','"
-                                    + pincode.getText().toString() + "')");
-                    preparedStatement.executeUpdate();
-                    errorlbl.setText("Data Added successfully");
+                    st = connect.createStatement();
+                    ResultSet rs = st.executeQuery("select * from login where userid='" + edname.getText().toString() + "' and password='" + edpassword.getText().toString() + "'");
+                    if (rs != null && rs.next()) {
+                        Intent i = new Intent(Login.this, Explore.class);
+                        startActivity(i);
+                    } else {
+                        errorlbl.setText("Sorry, wrong credentials!!!");
+                    }
+
+
                 } catch (SQLException e) {
                     errorlbl.setText(e.getMessage().toString());
                 }
